e68b3c6a103333ed2a2d532cc7bcbb2cb6a970dd
Divina21 <36093237+Divina21@users.noreply.github.com>
2018-04-12 09:30:43 +0800

    Create sister.php

---
+++ sister.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>My Family</title>
+<style type="text/css">
+body{
+    font-family:verdana,arial,san-serif;
+    margin:30px;
+    background-color:pink;
+   }
+
+</style>
+</head>
+<body>
+<h1>SISTER's</h1>
+<p><a href="diane.html" target="content">DIANE DE JESUS</a></p>
+<p><a href="dyar.html" target="content">DYAR DE JESUS</a></p>
+<p><a href="dolly.html" target="content">DOLLY DE JESUS</a></p>
+<p><a href="content.html" target="content">HOME</a></p>
+</body>
+</html>
