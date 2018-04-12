cd1455d13560510609dcbeec296923446f340a7a
Divina21 <36093237+Divina21@users.noreply.github.com>
2018-04-12 09:41:45 +0800

    Create friend.php

---
+++ friend.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>My Family</title>
+<style type="text/css">
+body{
+    font-family:verdana,arial,san-serif;
+    font-size:10pt;
+	margin:30px;
+    background-color:#ff9900;
+   }
+
+</style>
+</head>
+<body>
+<h1>FRIENDS</h1>
+<p><a href="jeniva.php" target="content">JENIVA DELFIN</a></p>
+<p><a href="justin.php" target="content">JUSTIN DELFIN</a></p>
+<p><a href="daniel.php" target="content">DANIEL DELFIN</a></p>
+<p><a href="daphne.php" target="content">DAPHNE PARAN</a></p>
+<p><a href="jesrel.php" target="content">JESREL DELFIN</a></p>
+<p><a href="joven.php" target="content">JOVEN DELFIN</a></p>
+</body>
+</html>
